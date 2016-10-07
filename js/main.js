7f541659c88f0b046098e9c6093806b84aa49b8a
YuriyChaban <shuyterrush@gmail.com>
2016-10-07 10:37:09 +0300

    finish page index.html

--- js/main.js
+++ js/main.js
@@ -46,22 +46,6 @@ $(function () {
     });
 });
 
-// for slider in Home page
-var swiper_main = new Swiper('.swiper-container-home-page', {
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 0,
-    simulateTouch: true,
-    centeredSlides: true,
-    autoplay: 3500,
-    speed: 1000,
-    watchSlidesProgress: true,
-    autoplayDisableOnInteraction: false
-});
 // mobile blog more slider
 var swiper = new Swiper('.swiper-container-mobile', {
     paginationClickable: true,
